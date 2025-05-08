140dd55eb19f946621f39eb341f3c678668c8f14
Angelica Muñoz <maria1020483@gmail.com>
2025-05-07 19:07:49 -0500

    primer commit

---
+++ src/main/java/com/marias/LasMariasApplication.java
@@ -0,0 +1,13 @@
+package com.marias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LasMariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LasMariasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marias/LasMariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LasMariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
